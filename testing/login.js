40ea20e3595bd895dfb6e1a7113f9404d42268b9
Dhruv Saxena <dhruv162002@gmail.com>
2022-06-11 22:44:11 +0530

    Reorganized

--- testing/login.js
+++ testing/login.js
@@ -1,21 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
-import {getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js";
-import { getFirestore, doc, getDoc, getDocs, collection, addDoc, setDoc} from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCFKct3s9R_3NmHh49mnZl1U_WcKcN1Nuo",
-  authDomain: "attendance-management-sy-2b849.firebaseapp.com",
-  projectId: "attendance-management-sy-2b849",
-  storageBucket: "attendance-management-sy-2b849.appspot.com",
-  messagingSenderId: "533410295224",
-  appId: "1:533410295224:web:d339ea8ee080698bd4334d"
-};
-
+import {firebaseConfig, initializeApp, getAuth, createUserWithEmailAndPassword, getFirestore, doc, getDoc, getDocs, collection, addDoc, setDoc} from "./config.js"
 
 var email = prompt("Enter your email");
 var password = prompt("Enter your password");
